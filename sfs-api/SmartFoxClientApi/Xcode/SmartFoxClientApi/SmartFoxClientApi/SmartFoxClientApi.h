724e06b516746f3527a86b20cd0f6fd73d4575ff
Dang Hoang Tuan Anh <dh.tuananh.91@gmail.com>
2017-02-03 00:44:37 +0700

    + Added SFS Xcode project

---
+++ sfs-api/SmartFoxClientApi/Xcode/SmartFoxClientApi/SmartFoxClientApi/SmartFoxClientApi.h
@@ -0,0 +1,13 @@
+//
+//  SmartFoxClientApi.h
+//  SmartFoxClientApi
+//
+//  Created by Dang Hoang Tuan Anh on 1/28/17.
+//  Copyright © 2017 Dang Hoang Tuan Anh. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SmartFoxClientApi : NSObject
+
+@end
